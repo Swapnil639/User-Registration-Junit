4952a576ad62efee4b8283522452cd1378400ec9
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-10 17:08:38 +0530

    Added :User need to enter valid Email id

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -9,6 +9,7 @@ public class UserRegistration {
     Scanner scanner=new Scanner(System.in);
 
     public void firstName(){
+        System.out.println("Enter First name:");
         String firstName=scanner.next();
         Pattern pattern=Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Matcher matcher=pattern.matcher(firstName);
@@ -19,9 +20,21 @@ public class UserRegistration {
         }
     }
     public void lastName(){
+        System.out.println("Enter Last name:");
         String lastName=scanner.next();
         Pattern pattern=Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Matcher matcher=pattern.matcher(lastName);
+        if (matcher.matches()){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Not valid");
+        }
+    }
+    public void emailId(){
+        System.out.println("Enter the Email Id:");
+        String email=scanner.next();
+        Pattern pattern=Pattern.compile("^[a-z0-9]{2,}+@[a-z]{2,}+.[a-z]{2,}$");
+        Matcher matcher=pattern.matcher(email);
         if (matcher.matches()){
             System.out.println("Valid");
         }else {
@@ -32,6 +45,7 @@ public class UserRegistration {
        UserRegistration userRegistration=new UserRegistration();
        userRegistration.firstName();
        userRegistration.lastName();
+       userRegistration.emailId();
     }
 
 }
